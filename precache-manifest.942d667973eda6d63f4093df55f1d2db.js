54b772d9c79225ac17977355f6686b4235f35c52
Natalia Pavlova <natalja1912@yandex.ru>
2020-10-04 22:17:51 +0300

    Updates

---
+++ precache-manifest.942d667973eda6d63f4093df55f1d2db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a35354ab612f8aa6d69a6b10ae5ad2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cfcac1b3245b0961aaa3",
+    "url": "./static/css/main.3f1e9f06.chunk.css"
+  },
+  {
+    "revision": "792b3bd77e18507ff510",
+    "url": "./static/js/2.c33daf7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c33daf7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcac1b3245b0961aaa3",
+    "url": "./static/js/main.2734a997.chunk.js"
+  },
+  {
+    "revision": "b12fb06c1b19e2084157",
+    "url": "./static/js/runtime-main.e89f0be9.js"
+  }
+]);

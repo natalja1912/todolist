0de9a088c8cef516e57f0aeb13c906d566cad1f7
Natalia Pavlova <natalja1912@yandex.ru>
2020-10-06 17:30:30 +0300

    fix: adaptivity

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './Todo.css';
-import EditIcon from '../../icons/EditIcon';
-import CopyIcon from '../../icons/CopyIcon';
-import DeleteIcon from '../../icons/DeleteIcon';
 import Checkbox from '../Checkbox/Checkbox';
 
 function Todo({ todo, index, handleEditButton, handleCopyButton, handleDeleteButton, handleCheckBox }) {
@@ -10,15 +7,13 @@ function Todo({ todo, index, handleEditButton, handleCopyButton, handleDeleteBut
     return (
         <li className="list__item" key={index}>
             <div className="list__item-text">
-                <label className={`list__item-label ${todo.checked && 'label-strike'}`}>
-                    <Checkbox checked={todo.checked} onClick={() => handleCheckBox(index)} />
-                    {todo.task}
-                </label>
+                <Checkbox checked={todo.checked} onClick={() => handleCheckBox(index)} />
+                <p className={`list__item-label ${todo.checked && 'label-strike'}`}>{todo.task}</p>
             </div>
             <div className="list__item-buttons">
-                <button className="list__item-button" onClick={() => handleEditButton(index)}><EditIcon /></button>
-                <button className="list__item-button" onClick={() => handleCopyButton(index)}><CopyIcon /></button>
-                <button className="list__item-button" onClick={() => handleDeleteButton(index)}><DeleteIcon /></button>
+                <button className="list__item-button list__item-button_edit" onClick={() => handleEditButton(index)}></button>
+                <button className="list__item-button list__item-button_copy" onClick={() => handleCopyButton(index)}></button>
+                <button className="list__item-button list__item-button_delete" onClick={() => handleDeleteButton(index)}></button>
             </div>
         </li>
     )
